a710389645eecb5b55ba6f568503cb8328058014
0-0 <39878411+reighpuy@users.noreply.github.com>
2020-08-13 14:50:22 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+"use strict";
+var $ = document.querySelectorAll.bind(document);
+$(".focusable, .button").forEach(function (el) {
+  // blur only on mouse click
+  // for accessibility, keep focus when keyboard focused
+  el.addEventListener("mousedown", function (e) {return e.preventDefault();});
+  el.setAttribute("tabindex", "0");
+});
+$(".server").forEach(function (el) {
+  el.addEventListener("click", function () {
+    var activeServer = $(".server.active")[0];
+    activeServer.classList.remove("active");
+    activeServer.removeAttribute("aria-selected");
+    el.classList.add("active");
+    el.setAttribute("aria-selected", true);
+  });
+});
+$(".channel-text").forEach(function (el) {
+  el.addEventListener("click", function () {
+    $(".channel-text.active")[0].classList.remove("active");
+    el.classList.add("active");
+  });
+});
+// focus/blur on channel header click
+$(".channels-header")[0].addEventListener("click", function (e) {
+  e.preventDefault();
+  var focused = document.activeElement === e.target;
+  focused ? e.target.blur() : e.target.focus();
+});
